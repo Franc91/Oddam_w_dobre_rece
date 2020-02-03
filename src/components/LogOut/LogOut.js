8e3f7b9250bf575a5f39c5df0c8f2aa3c29d1243
Franc91 <franczakszymon@gmail.com>
2020-02-03 22:16:26 +0100

    dodano zmiane navbar przy zalogowaniu, dodano logout

--- src/components/LogOut/LogOut.js
+++ src/components/LogOut/LogOut.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Nav from '../Home/Nav/Nav'
+import Menu from '../Home/Menu'
+import { Link } from 'react-router-dom'
 
 export const LogOut = () => {
     return (
-        <div>
-            LogOut
-        </div>
+        <>
+            <div>
+                <Nav/>
+                <Menu/>
+            </div>
+            <div>
+                <h1>Wylogowanie nastąpiło pomyślnie</h1>
+                <div>__________________________________________________</div>
+                <button><Link to='/'>Strona Główna</Link></button>
+            </div>
+
+        </>
     )
 }
 
